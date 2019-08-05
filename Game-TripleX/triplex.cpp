c9a09ed250de18297f8db768a9643400ce9c5973
conganh176 <40571925+conganh176@users.noreply.github.com>
2019-08-05 19:55:24 +0700

    .

--- Game-TripleX/triplex.cpp
+++ Game-TripleX/triplex.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 
-void PrintIntroduction()
+void PrintIntroduction(int Difficult)
 {
-    std::cout << "\nYou are an adventurer going through the dungeon...\n";
+    std::cout << "\nThou art an adventurer going through the dungeon...\n";
     std::cout << "In the end of the dungeon, behind the sealed door there are huge treasures for someone who is worthy...\n";
+    std::cout << "Door number " << Difficult << std::endl;
     std::cout << "Solve the puzzle by entering the right number, the incorrect numbers will result with the consequence...\n\n";
 }
 
-bool PlayGame()
+bool PlayGame(int Difficult)
 {
-    PrintIntroduction();
+    PrintIntroduction(Difficult);
 
     const int Number1 = 4;
     const int Number2 = 3;
@@ -32,25 +33,33 @@ bool PlayGame()
 
     if (GuessSum == Sum && GuessProduct == Product)
     {
-        std::cout << "You win!\n";
+        std::cout << "\nThou solved the puzzled. The upcomming is waiting for thou...\n";
         return true;
     }
     else
     {
-        std::cout << "You lose!\n";
+        std::cout << "\nThou's life journey has come to an end...\n";
         return false;
     }
 }
 
 int main()
 {
-    while (true)
+    int Difficult = 1;
+    const int MaxDifficult = 5;
+    while (Difficult <= MaxDifficult)
     {
-        bool bLevelCompleted = PlayGame();
+        bool bLevelCompleted = PlayGame(Difficult);
         std::cin.clear();
         std::cin.ignore();
+
+        if (bLevelCompleted)
+        {
+            ++Difficult;
+        }
     }
     // system("pause");
 
+    std::cout << "Well done! Thou solved all the puzzle doors. The treasures will belong to the worthy. And that shalt be thou.";
     return 0;
 }
