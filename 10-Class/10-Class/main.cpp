1e8622c24c715c7dede3e4d0921b7cb1e61049dd
Cong Anh Nguyen <sworlds@Congs-MacBook-Air.local>
2020-07-14 19:31:34 +0700

    .

--- 10-Class/10-Class/main.cpp
+++ 10-Class/10-Class/main.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
 
+class Player {
+private:
+    std::string name {"Player"};
+    int health;
+    int xp;
+public:
+    void talk(std::string text) {
+        std::cout << name << " says " << text << std::endl;
+    }
+    bool is_dead();
+};
+
+class Account {
+private:
+    std::string name;
+    double balance;
+    
+public:
+    void deposit(double bal) {
+        balance += bal;
+        std::cout << "In deposit" << std::endl;
+    }
+    void withdraw(double bal) {
+        balance -= bal;
+        std::cout << "In withdraw" << std::endl;
+    }
+};
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Player frank;
+//    frank.name = "Frank";
+//    frank.health = 100;
+//    frank.xp = 20;
+    frank.talk("Hello");
+    
+    Player *enemy = new Player;
+//    (*enemy).name = "Enemy";
+//    (*enemy).health = 100;
+//    enemy->xp = 15;
+    enemy->talk("Sup'");
+    delete enemy;
+    
+    Account frank_account;
+//    frank_account.name = "Frank's account";
+//    frank_account.balance = 5000.0;
+    frank_account.deposit(1000.0);
+    frank_account.withdraw(500.0);
+    
     return 0;
 }
